fb0eee18ae1d0d389489ea703cd37c7579f707f8
KD3-80142 <ruturajmane1999@gmail.com>
2024-04-17 10:04:14 +0530

    Fetch MongoDB data  and design frontend to disply appropriate OUTPUT

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+const express = require('express');
+
 const mongoose = require('mongoose');
 
+const app = express();
 
 const mongoURI = 'mongodb://localhost:27017/Data';
 
@@ -10,3 +13,29 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     
   })
   .catch(err => console.error('Error connecting to MongoDB:', err));
+
+  const dataSchema = new mongoose.Schema({
+    ts: Date,
+    machine_status:Number,
+    vibration: Number
+});
+
+
+const Data = mongoose.model('Data', dataSchema);
+
+
+app.get('/api/data', async (req, res) => {
+    try {
+        const data = await Data.find();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
